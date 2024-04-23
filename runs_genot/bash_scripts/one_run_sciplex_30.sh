1616b0a8f50ad5f15fe5d22a6763686c23cd3910
Dominik Klein <domin.klein@gmail.com>
2024-04-23 10:46:09 +0200

    update

---
+++ runs_genot/bash_scripts/one_run_sciplex_30.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH -o one_sciplex.out
+
+#SBATCH -e one_sciplex.err
+
+#SBATCH -J one_sciplex
+
+#SBATCH -p gpu_p
+
+#SBATCH --qos=gpu_normal
+
+#SBATCH --constraint=a100_80gb
+
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=160G
+
+#SBATCH -t 1-00:00
+
+#SBATCH --nice=1
+
+source ${HOME}/.bashrc_new
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate /home/icb/dominik.klein/mambaforge/envs/ot_pert_genot
+
+python /home/icb/dominik.klein/git_repos/ot_pert_new/runs_genot/train_sciplex.py dataset=sciplex_biolord_split model=sciplex training=training_sciplex logger=combosciplex
