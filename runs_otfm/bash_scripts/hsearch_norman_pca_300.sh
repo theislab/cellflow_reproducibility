8483fb1071706c24f75c3d208f52c790c7c6b6af
Sören Becker <soeren.becker@hpc-build01.scidom.de>
2024-10-20 12:35:37 +0200

    setup hydra configs for gridsearch on norman dataset

---
+++ runs_otfm/bash_scripts/hsearch_norman_pca_300.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH -o h-otfm-norman.out
+
+#SBATCH -e h-otfm-norman.err
+
+#SBATCH -J h-otfm-norman
+
+#SBATCH -p gpu_p
+
+#SBATCH --qos=gpu_normal
+
+#SBATCH --gres=gpu:1
+
+#SBATCH --mem=80G
+
+#SBATCH -t 0-01:00:00
+
+#SBATCH --nice=1
+
+source /home/haicu/soeren.becker/.bashrc
+conda activate /home/haicu/soeren.becker/miniconda3/envs/env_cfp
+
+python /home/haicu/soeren.becker/repos/ot_pert_reproducibility/runs_otfm/train_norman_pca_300.py \
+    --multirun \
+    dataset=norman \
+    +hparams_search=hparams_norman \
+    model=norman \
+    training=norman \
+    logger=norman
---
+++ runs_otfm/train_norman_pca_50.py
@@ -0,0 +1,123 @@
+import functools
+import os
+import sys
+import traceback
+from typing import Dict, Literal, Optional, Tuple
+
+import cfp
+import scanpy as sc
+import numpy as np
+import functools
+from ott.solvers import utils as solver_utils
+import optax
+from omegaconf import OmegaConf
+from typing import NamedTuple, Any
+import hydra
+import wandb
+
+
+
+
+@hydra.main(config_path="conf", config_name="train")
+def run(config):
+    config_dict  = OmegaConf.to_container(config, resolve=True)
+    print(config_dict)
+    split = config_dict["dataset"]["split"]
+    adata_train_path = f"/home/haicu/soeren.becker/repos/ot_pert_reproducibility/norman2019/norman_preprocessed_adata/adata_train_pca_50_split_{split}.h5ad"
+    adata_test_path = f"/home/haicu/soeren.becker/repos/ot_pert_reproducibility/norman2019/norman_preprocessed_adata/adata_val_pca_50_split_{split}.h5ad"
+    adata_ood_path = f"/home/haicu/soeren.becker/repos/ot_pert_reproducibility/norman2019/norman_preprocessed_adata/adata_test_pca_50_split_{split}.h5ad"
+    adata_train = sc.read_h5ad(adata_train_path)
+    adata_test = sc.read_h5ad(adata_test_path)
+    adata_ood = sc.read_h5ad(adata_ood_path)
+    
+    cf = cfp.model.CellFlow(adata_train, solver="otfm")
+    
+    # Prepare the training data and perturbation conditions
+    perturbation_covariates = {k: tuple(v) for k, v in config_dict["dataset"]["perturbation_covariates"].items()}
+    
+    cf.prepare_data(
+        sample_rep="X_pca",
+        control_key="control",
+        perturbation_covariates=perturbation_covariates,
+        perturbation_covariate_reps=dict(config_dict["dataset"]["perturbation_covariate_reps"]),
+        sample_covariates=None,
+        sample_covariate_reps=None,
+        split_covariates=None
+    )
+    
+    match_fn = functools.partial(
+        solver_utils.match_linear,
+        epsilon=config_dict["model"]["epsilon"],
+        scale_cost="mean",
+        tau_a=config_dict["model"]["tau_a"],
+        tau_b=config_dict["model"]["tau_b"]
+    )
+    
+    optimizer = optax.MultiSteps(optax.adam(config_dict["model"]["learning_rate"]), config_dict["model"]["multi_steps"])
+    flow = {config_dict["model"]["flow_type"]: config_dict["model"]["flow_noise"]}
+
+    layers_before_pool = config_dict["model"]["layers_before_pool"]
+    layers_after_pool = config_dict["model"]["layers_after_pool"]
+    
+    # Prepare the model
+    cf.prepare_model(
+        encode_conditions=True,
+        condition_embedding_dim=config_dict["model"]["condition_embedding_dim"],
+        pooling=config_dict["model"]["pooling"],
+        time_encoder_dims=config_dict["model"]["time_encoder_dims"],
+        time_encoder_dropout=config_dict["model"]["time_encoder_dropout"],
+        hidden_dims=config_dict["model"]["hidden_dims"],
+        hidden_dropout=config_dict["model"]["hidden_dropout"],
+        decoder_dims=config_dict["model"]["decoder_dims"],
+        decoder_dropout=config_dict["model"]["decoder_dropout"],
+        layers_before_pool=layers_before_pool,
+        layers_after_pool=layers_after_pool,
+        cond_output_dropout=config_dict["model"]["cond_output_dropout"],
+        time_freqs=config_dict["model"]["time_freqs"],
+        match_fn=match_fn,
+        optimizer=optimizer,
+        flow=flow,
+        layer_norm_before_concatenation=config_dict["model"]["layer_norm_before_concatenation"],
+        linear_projection_before_concatenation=config_dict["model"]["linear_projection_before_concatenation"],
+    )
+    
+    cf.prepare_validation_data(
+        adata_test,
+        name="test",
+        n_conditions_on_log_iteration=config_dict["training"]["test_n_conditions_on_log_iteration"],
+        n_conditions_on_train_end=config_dict["training"]["test_n_conditions_on_log_iteration"],
+    )
+    
+    cf.prepare_validation_data(
+        adata_ood,
+        name="ood",
+        n_conditions_on_log_iteration=2,#config_dict["training"]["ood_n_conditions_on_log_iteration"],
+        n_conditions_on_train_end=2,#config_dict["training"]["ood_n_conditions_on_log_iteration"],
+    )
+    
+    metrics_callback = cfp.training.Metrics(metrics=["r_squared", "mmd", "e_distance"])
+    decoded_metrics_callback = cfp.training.PCADecodedMetrics(ref_adata=adata_train, metrics=["r_squared", "mmd", "e_distance"])
+    wandb_callback = cfp.training.WandbLogger(project="cfp_otfm_norman", out_dir="/home/icb/dominik.klein/tmp", config=config_dict)
+
+    callbacks = [metrics_callback, decoded_metrics_callback, wandb_callback]
+    
+    cf.train(
+        num_iterations=config_dict["training"]["num_iterations"],
+        batch_size=config_dict["training"]["batch_size"],
+        callbacks=callbacks,
+        valid_freq=config_dict["training"]["valid_freq"],
+    )
+    
+    if config_dict["training"]["save_model"]:
+        cf.save(config_dict["training"]["out_dir"], file_prefix=wandb.run.name)
+
+    
+    return 1.0
+
+if __name__ == "__main__":
+    try:
+        run()
+    except Exception as e:
+        print(f"An error occurred: {e}", file=sys.stderr)
+        traceback.print_exc()
+        sys.exit(1)
